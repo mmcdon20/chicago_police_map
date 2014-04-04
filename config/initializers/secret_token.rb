11ba780dbcabe2bb0013a4461fb6da0afe51d922
mmcdon20 <mmcdon20@live.com>
2014-04-04 01:57:58 -0500

    generated rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ChicagoPoliceMap::Application.config.secret_key_base = '85c5d48a19a534a5be4fcc043b1768de7782d62795f353b83f7a58456e6b81cb7e53819cf9ad30d00e8de12d2bd086557a9bfd2055abc4609e1a58755ea5360d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ChicagoPoliceMap::Application.config.session_store :cookie_store, key: '_chicago_police_map_session'
