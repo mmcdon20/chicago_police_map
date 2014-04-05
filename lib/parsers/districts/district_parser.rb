17e9f6d5a0e91a813eb74b41795dcdd5170fe357
mmcdon20 <mmcdon20@live.com>
2014-04-05 00:54:43 -0500

    added files to reformat police district json data

---
+++ lib/parsers/districts/district_parser.rb
@@ -0,0 +1,23 @@
+require 'json'
+require 'rexml/document'
+
+file = open('districts.json')
+json = JSON.parse(file.read)
+
+result = 'var policeData = {"type":"FeatureCollection","features":[' + " \n"
+
+json['features'].each do |feature|
+  coordinates = feature['geometry']['geometries'][1].to_s.delete(' ').gsub('=>',':')
+  xml         = REXML::Document.new('<xml>'+feature['properties']['Description']+'</xml>')
+  items       = xml.elements.to_a('//span')
+  district    = items[1][0].to_s
+  beat        = items[7][0].to_s
+  result += "{\"type\":\"Feature\",\"properties\":{\"district\":\"#{district}\",\"beat\":\"#{beat}\"},\"geometry\":#{coordinates}},\n"
+end
+
+result.chop!
+result.chop!
+
+result += ']}'
+
+File.open('police.js', 'w') { |file| file.write(result) }
