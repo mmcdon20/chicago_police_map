fe95c5cb4914676d4da7a31e59ea523845388945
mmcdon20 <mmcdon20@live.com>
2014-04-06 04:17:15 -0500

    updated controller to give additional data on crimes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,18 @@
 class WelcomeController < ApplicationController
   def index
-    @count = Crime.count
+    require 'json'
+
+    items = Crime.select(:beat).map(&:beat)
+
+    @beats = items.uniq.sort
+
+    @frequency = items.reduce(Hash.new(0)) do |beat_count, beat|
+      beat_count[beat] += 1
+      beat_count
+    end
+
+    max = @frequency.values.max
+
+    @levels = [(max*1.0), (max*0.8), (max*0.6), (max*0.4), (max*0.2)].map(&:to_i)
   end
 end
