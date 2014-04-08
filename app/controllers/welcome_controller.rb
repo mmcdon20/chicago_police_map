6b8f0fe0677f459b92cd3bbdd592a7f8ed0b0a6b
mmcdon20 <mmcdon20@live.com>
2014-04-08 13:53:11 -0500

    updated controller to render a partial for crimes rather than a full page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -19,7 +19,7 @@ class WelcomeController < ApplicationController
   def crimes
     @crimes = Crime.limit(10).offset(params[:offset]).where(beat:params[:beat])
     respond_to do |format|
-      format.html
+      format.html { render partial:'/welcome/crimes' }
       format.js
     end
   end
