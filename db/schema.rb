79127ed7a726b15c472f398c02d1e1b75dadbb6c
mmcdon20 <mmcdon20@live.com>
2014-04-18 13:38:42 -0500

    added beat model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140405182610) do
+ActiveRecord::Schema.define(version: 20140418174716) do
+
+  create_table "beats", force: true do |t|
+    t.string   "beat"
+    t.integer  "crimes"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "crimes", force: true do |t|
     t.datetime "date"
